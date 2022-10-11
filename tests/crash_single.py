ca11b38e8dcc2d1a72565b6ede98d6935f1fcffe
Jakob Unterwurzacher <jakobunt@gmail.com>
2022-10-11 21:29:44 +0200

    tests: add crash_single

---
+++ tests/crash_single.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import unhandled_exit
+
+unhandled_exit.activate()
+
+x = 1 / 0
--- tests/tests.py
+++ tests/tests.py
@@ -9,3 +9,8 @@ def test_crash_main():
 def test_crash_thread():
     ret = os.system("timeout -v 10 ./tests/crash_thread.py") >> 8
     assert ret == 1
+
+
+def test_crash_single():
+    ret = os.system("timeout -v 10 ./tests/crash_single.py") >> 8
+    assert ret == 1
